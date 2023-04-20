1d3ca0c5d7eea8f423a36c6ffe8bb6e52a97454f
Vincent Turlier <56164263+Arkohn@users.noreply.github.com>
2023-04-21 00:29:14 +0200

    UGH

--- src/main/java/com/example/carRental/CarRentalApplication.java
+++ src/main/java/com/example/carRental/CarRentalApplication.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -35,29 +40,36 @@ public class CarRentalApplication {
 			System.out.println("-------------------------------");
 			System.out.println("Car found with findAll():");
 			
-			List<String> Producer = new ArrayList<String>();
-			Producer.add("cmd.exe");
-			Producer.add("/c");
-			Producer.add("docker exec --interactive --tty kafka-broker kafka-console-producer --bootstrap-server kafka:9092 --topic quickstart");
+			KafkaProducer<String, String> producer = new KafkaProducer<>(setProperties());
 
 			for (Car c : carRepository.findAll()) {
-				Producer.add(c.toString());
+				// create a producer record
+				ProducerRecord<String, String> producerRecord = new ProducerRecord<>("topic", c.toString());
+				
+				// send data - asynchronous
+				producer.send(producerRecord);
 				System.out.println(c.toString());
 			}
-			
-			String[] Prodstr = (String[]) Producer.toArray();
-			
-			ProcessBuilder pb = new ProcessBuilder(Prodstr);
-			try {
-				pb.start();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-
+			// flush data - synchronous
+			producer.flush();
+        
+			// flush and close producer
+			producer.close();
 
 		};
+	}
+
+	public Properties setProperties(){
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+		// create Producer properties
+        Properties properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		return properties;
 	}
 
 }
