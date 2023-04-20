2c2094110f07e780584633365dd223ec2fdc0ac0
Vincent Turlier <56164263+Arkohn@users.noreply.github.com>
2023-04-20 23:38:48 +0200

    Added cmd prompts to automate topic & producer

--- src/main/java/com/example/carRental/CarRentalApplication.java
+++ src/main/java/com/example/carRental/CarRentalApplication.java
@@ -1,18 +1,26 @@
 package com.example.carRental;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.LongSerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import java.util.Properties;
 
 @SpringBootApplication
 public class CarRentalApplication {
 
 	public static void main(String[] args) {
+		String generateTopic = "docker exec --interactive --tty kafka-broker kafka-topics --bootstrap-server kafka:9092 --create --topic quickstart";
+		ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", generateTopic);
+		try {
+			pb.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		SpringApplication.run(CarRentalApplication.class, args);
 	}
 
@@ -26,10 +34,28 @@ public class CarRentalApplication {
 
 			System.out.println("-------------------------------");
 			System.out.println("Car found with findAll():");
+			
+			List<String> Producer = new ArrayList<String>();
+			Producer.add("cmd.exe");
+			Producer.add("/c");
+			Producer.add("docker exec --interactive --tty kafka-broker kafka-console-producer --bootstrap-server kafka:9092 --topic quickstart");
 
 			for (Car c : carRepository.findAll()) {
+				Producer.add(c.toString());
 				System.out.println(c.toString());
 			}
+			
+			String[] Prodstr = (String[]) Producer.toArray();
+			
+			ProcessBuilder pb = new ProcessBuilder(Prodstr);
+			try {
+				pb.start();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
 
 		};
 	}
