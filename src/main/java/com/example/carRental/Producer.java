8c2b94efbe5ff54eac600eef6a3f91acbe0abfbf
Vincent Turlier <56164263+Arkohn@users.noreply.github.com>
2023-04-21 10:16:17 +0200

    updated stuff

--- src/main/java/com/example/carRental/Producer.java
+++
@@ -1,6 +0,0 @@
-package com.example.carRental;
-
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
